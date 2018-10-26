2ee0c9cd706d1d5eb3519e72f6d9ccb6cd3b7f6a
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2018-10-26 10:00:28 -0300

    Adicionando arquivos

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 	/* Preencha com informações do seu banco de dados */
 	define('DB_HOSTNAME', 'localhost');
-	define('DB_USERNAME', 'mateus');
-	define('DB_PASSWORD', 'Mateus@123');
-	define('DB_DATABASE', 'disbecol');
+	define('DB_USERNAME', 'seu_usuario');
+	define('DB_PASSWORD', 'sua_senha');
+	define('DB_DATABASE', 'sua_tabela');
 	define('DB_CHARSET', 'utf8')
 
 ?>
